c9cacdf9d9f39383bb85baa862de616e697afab7
Justin <biggreenskiing@gmail.com>
2018-12-30 09:26:34 -0700

    changed log level to see if that shows in VC

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -5,7 +5,7 @@ public func boot(_ app: Application) throws {
     // your code here
     
     let logger = try app.make(Logger.self)
-    logger.info("testing the logging system")
+    logger.error("testing the logging system")
     
     let fisURLString = "ftp://ftp.fisski.com/Software/Files/Fislist/ALFP919F.zip"
     if let fisURL = URL(string: fisURLString) {
@@ -13,12 +13,12 @@ public func boot(_ app: Application) throws {
         let data = try Data(contentsOf: fisURL)
         if #available(OSX 10.12, *) {
             try data.write(to: FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("vaporTestDownload.zip"))
-            logger.info("finished writing ftp download to disk")
+            logger.error("finished writing ftp download to disk")
         } else {
             // Fallback on earlier versions
-            logger.info("FileManager.default.homeDirectoryForCurrentUser not available on this platform")
+            logger.error("FileManager.default.homeDirectoryForCurrentUser not available on this platform")
         }
         
-        logger.info("download complete")
+        logger.error("download complete")
     }
 }
