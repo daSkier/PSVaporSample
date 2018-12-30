276f24e6f2014aa6fdfb912880b4af3d042f6432
Justin <biggreenskiing@gmail.com>
2018-12-30 08:12:52 -0700

    adds logging

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,18 +3,22 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
+    
+    let logger = try app.make(Logger.self)
+    logger.info("testing the logging system")
+    
     let fisURLString = "ftp://ftp.fisski.com/Software/Files/Fislist/ALFP919F.zip"
     if let fisURL = URL(string: fisURLString) {
         /// Download file via FTP
         let data = try Data(contentsOf: fisURL)
         if #available(OSX 10.12, *) {
             try data.write(to: FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("vaporTestDownload.zip"))
-            print("finished writing ftp download to disk")
+            logger.info("finished writing ftp download to disk")
         } else {
             // Fallback on earlier versions
-            print("FileManager.default.homeDirectoryForCurrentUser not available on this platform")
+            logger.info("FileManager.default.homeDirectoryForCurrentUser not available on this platform")
         }
         
-        print("download complete")
+        logger.info("download complete")
     }
 }
