a4ea8e6d244960a9a143832f9e4a761c55d1df79
Justin <biggreenskiing@gmail.com>
2018-12-30 20:45:22 -0700

    replaces boot.swift sample download code

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -4,27 +4,12 @@ import Vapor
 public func boot(_ app: Application) throws {
     // your code here
     
-    let logger = try app.make(Logger.self)
-    logger.info("testing the logging system")
-    
-    let fisURLString = "ftp://ftp.fisski.com/Software/Files/Fislist/ALFP919F.zip"
-    if let fisURL = URL(string: fisURLString) {
-        
-        /// Download file via FTP
-        logger.info("starting to download data from URL")
-        let data = try Data(contentsOf: fisURL)
-        logger.info("Completed download of data from URL")
-        
-        ///testing
-        if #available(OSX 10.12, *) {
-            logger.info("will attempt to write to disk")
-            try data.write(to: FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("vaporTestDownload.zip"))
-            logger.info("finished writing ftp download to disk")
-        } else {
-            // Fallback on earlier versions
-            logger.info("FileManager.default.homeDirectoryForCurrentUser not available on this platform")
-        }
-        
-        logger.info("download complete")
+    var configuration = SessionConfiguration()
+    configuration.host = "ftp://ftp.fisski.com:21"
+    let session = Session(configuration: configuration)
+    print("starting download")
+    session.download("/Software/Files/Fislist/ALFP919F.zip") {
+        (fileURL, error) -> Void in
+        print("Download file with result:\n\(fileURL), error: \(error)\n\n")
     }
 }
