fc3d2fdb608513743ffbedea02e253f83f3bfd32
Justin <biggreenskiing@gmail.com>
2019-01-05 19:18:30 -0700

    removes all ccurl in an attempt to fix crashes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,5 @@
 import Vapor
-import CCurl
+//import CCurl
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -15,15 +15,12 @@ public func routes(_ router: Router) throws {
         ])
     }
     
-//    router.get("updatedLists") { req -> Future<Int> in
-//        
-//        let logger = try req.make(Logger.self)
-//        logger.info("testing the logging system")
-//        
-//        let fisURLString = "ftp://ftp.fisski.com/Software/Files/Fislist/ALFP919F.zip"
-//        if let fisURL = URL(string: fisURLString) {
-//         return try
-//        }
-//        return try
-//    }
+    router.get("updatedLists") { req -> Future<View> in
+        
+        let logger = try req.make(Logger.self)
+        logger.info("testing the logging system")
+        
+        return try req.view().render("hello", ["name": req.parameters.next(String.self)
+            ])
+    }
 }
