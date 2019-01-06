00e082261105e8decdc300eac8c8be9f092f1e6d
Justin <biggreenskiing@gmail.com>
2019-01-05 17:35:41 -0700

    fixes after removal of SwiftyFTP

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -5,14 +5,14 @@ import CCurl
 public func boot(_ app: Application) throws {
     // your code here
     
-    var configuration = SessionConfiguration()
-    configuration.host = "ftp://ftp.fisski.com:21"
-    let session = Session(configuration: configuration)
-    print("starting download")
-    session.download("/Software/Files/Fislist/ALFP919F.zip") {
-        (fileURL, error) -> Void in
-        print("Download file with result:\n\(String(describing: fileURL)), error: \(String(describing: error))\n\n")
-    }
+//    var configuration = SessionConfiguration()
+//    configuration.host = "ftp://ftp.fisski.com:21"
+//    let session = Session(configuration: configuration)
+//    print("starting download")
+//    session.download("/Software/Files/Fislist/ALFP919F.zip") {
+//        (fileURL, error) -> Void in
+//        print("Download file with result:\n\(String(describing: fileURL)), error: \(String(describing: error))\n\n")
+//    }
     print("setting up curl objects")
     curl_global_init(Int(CURL_GLOBAL_DEFAULT))
     var curl = curl_easy_init()
